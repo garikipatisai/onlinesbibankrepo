c1d717183e5c1c1bf8a4c8ce7cf56ceb7cc9059e
garikipatisai <garikipatisaikrishna@gmail.com>
2017-11-10 13:36:37 -0800

    sbibankingapp commited

---
+++ sbibankingapp/src/main/java/net_banking/sbibankingapp/controller/HomeController.java
@@ -0,0 +1,19 @@
+package net_banking.sbibankingapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+@RequestMapping(value="/") 
+public ModelAndView home(){
+	
+	ModelAndView mav=new ModelAndView("home");
+	mav.addObject("page","home.jsp");
+	return mav;
+	
+}
+	
+	
+}
